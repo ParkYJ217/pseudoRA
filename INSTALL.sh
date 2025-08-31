40bf6dd0777acca2ae926a337f87073fc84229dc
ParkYJ217 <139526230+ParkYJ217@users.noreply.github.com>
2025-09-01 01:15:07 +0900

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -3,7 +3,7 @@ sudo apt install openjdk-8-jdk # can possibly work with higher versions
 
 ### Python modules.
 sudo apt install python3-pip python3-dev
-sudo -H python3 -m pip install pysam pandas biopython
+python3 -m pip install pysam pandas biopython
 
 ### Install third-party software
 sudo apt install bwa samtools  
